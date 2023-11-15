7164d85107ec78c116cb720e38a34409847a7d08
zidehsaraee <moein.zideh@gmail.com>
2023-11-15 14:06:45 +0000

    Update classes.js

--- classes.js
+++ classes.js
@@ -11,43 +11,32 @@ class Pokemon {
     }
 }
 
-class Pikachu extends Pokemon {
-    constructor() {
-        super("Pikachu", "eletric", 100, 60);
+
+// Normal Pokemon classes
+
+class NormalPokemon extends Pokemon {
+    constructor(name, type, health, power) {
+        super(name, type, health, power);
 
     }
 }
+const Pikachu = new NormalPokemon ("Pikachu", "eletric", 100, 60);
+const Charmander = new NormalPokemon ("Charmander", "fire", 100, 60);
+const Squirtle = new NormalPokemon ("Squirtle", "water", 100, 60);
+const Bulbasaur = new NormalPokemon ("Bulbasaur", "grass", 100, 60);
 
-class Charmander extends Pokemon {
-    constructor() {
 
-        super("Charmander", "fire", 100, 60);
-    }
-}
-
-class Squirtle extends Pokemon {
-    constructor() {
-
-        super("Squirtle", "water", 100, 60);
-
-    }
-}
-
-class Bulbasaur extends Pokemon {
-    constructor() {
-        super("Bulbasaur", "grass", 100, 60);
-
-    }
-}
+// Wild pokemon classes
 
 class WildPokemon extends Pokemon {
-    constructor() {
-        super("Mew", "psychic", 100, 60);
+    constructor(name, type, health, power) {
+        super(name, type, health, power);
     }
 }
+const Wildmew = new WildPokemon("Mew", "psychic", 100, 60);
+const Wildjynx = new WildPokemon("Jynx", "psychic", 100, 60);
+const Wildalakazam = new WildPokemon("Alakazam", "psychic", 100, 60);
+const Wildhypno = new WildPokemon("Hypno", "psychic", 100, 60);
 
-const jynx = new WildPokemon("Jynx", "psychic", 100, 60);
-const alakazam = new WildPokemon("Alakazam", "psychic", 100, 60);
-const hypno = new WildPokemon("Hypno", "psychic", 100, 60);
+module.export = {Pokemon, NormalPokemon, Pikachu, Bulbasaur, Squirtle, Charmander, WildPokemon, Wildmew, Wildjynx, Wildalakazam, Wildhypno};
 
-module.export = {Pokemon, Pikachu, Bulbasaur, Squirtle, Charmander, WildPokemon};
